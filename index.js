119490f5e3c52fa502a033eb95cd302a31a45d0e
Tikw <145377204+Tikkkkkw@users.noreply.github.com>
2025-01-15 22:34:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-document.getElementById("countel").innerText = 5
+
 //Built an counter app
+let count = 0;
+let savetext = "Previous Entries: "; 
+
+document.getElementById("countel").innerText = 5
+
+function increment() {
+    count++;
+    document.getElementById("countel").textContent = count;
+    console.log(count);
+}
+function save() {
+    savetext += count + " - "; 
+    document.getElementById("saves").innerText = savetext; 
+}
+
+
+
+
